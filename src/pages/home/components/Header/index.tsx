45307c45a3110ad797c5394034cb7e2baf542b72
josie <josephine.lucyrita@gmail.com>
2024-04-01 16:47:08 +0100

    changes section to header in Header component

--- src/pages/home/components/Header/index.tsx
+++ src/pages/home/components/Header/index.tsx
@@ -2,9 +2,9 @@ import './index.scss'
 
 const Header: React.FC = () => {
   return (
-    <section className="app-header">
+    <header className="app-header">
       <h1 className="app-header__title">Noughts and Crosses</h1>
-    </section>
+    </header>
   )
 }
 
