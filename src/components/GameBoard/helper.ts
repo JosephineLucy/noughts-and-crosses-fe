9b610e716b95fe4f5bb2c1e86e19030fe62b52f5
josie <josephine.lucyrita@gmail.com>
2024-05-08 17:20:51 +0100

    types return for createSpaceIds

--- src/components/GameBoard/helper.ts
+++ src/components/GameBoard/helper.ts
@@ -1,6 +1,6 @@
 import { NUM_OF_BOARD_SPACES } from '../../constants/constants'
 
-export function createSpaceIds() {
+export function createSpaceIds(): number[] {
   const result = []
   for (let i = 0; i < NUM_OF_BOARD_SPACES; i++) {
     result.push(i + 1)
