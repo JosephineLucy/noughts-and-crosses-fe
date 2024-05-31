cac05a5cc5e3431edac72b20025714d58b46cb15
josie <josephine.lucyrita@gmail.com>
2024-05-31 14:56:05 +0100

    changes section to fragment on Home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,3 @@
-import './styles.scss'
 import Footer from '../../components/Footer'
 import Game from '../../components/Game'
 import Header from '../../components/Header'
@@ -7,13 +6,13 @@ import { AppLabels, NUM_OF_BOARD_SPACES } from '../../constants/constants'
 const Home: React.FC = () => {
   const currentYear: string = String(new Date().getFullYear())
   return (
-    <div className="home">
+    <>
       <Header title={AppLabels.title} />
       <main>
         <Game numOfBoardSpaces={NUM_OF_BOARD_SPACES} />
       </main>
       <Footer title={`© ${AppLabels.title} ${currentYear}`} />
-    </div>
+    </>
   )
 }
 
