71c748a209274e3714db51d3a10d073e04741cba
josie <josephine.lucyrita@gmail.com>
2024-05-31 14:51:34 +0100

    changes section -> div on home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,13 +7,13 @@ import { AppLabels, NUM_OF_BOARD_SPACES } from '../../constants/constants'
 const Home: React.FC = () => {
   const currentYear: string = String(new Date().getFullYear())
   return (
-    <section className="home">
+    <div className="home">
       <Header title={AppLabels.title} />
       <main>
         <Game numOfBoardSpaces={NUM_OF_BOARD_SPACES} />
       </main>
       <Footer title={`© ${AppLabels.title} ${currentYear}`} />
-    </section>
+    </div>
   )
 }
 
